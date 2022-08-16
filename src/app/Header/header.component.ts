a0ce119499ed9c5af63f7da3acc211f5668eadda
Andrii Yelnikov <whiteyelnikov@gmail.com>
2022-08-16 23:27:43 +0300

    changed img path

--- src/app/Header/header.component.ts
+++ src/app/Header/header.component.ts
@@ -12,12 +12,12 @@ import { currencyObj } from "./type";
 export class HeaderComponent {
   currency: any;
   nameOfCurrency: {name: string, image: string}[] = [
-    {name: 'UAH', image: '.images/ua.svg'},
-    {name: 'EUR', image: 'eu.svg'},
-    {name: 'USD', image: 'images/us.svg'},
-    {name: 'GBP', image: 'images/gb.svg'},
-    {name: 'CAD', image: 'images/ca.svg'},
-    {name: 'JPY', image: 'images/jp.svg'},
+    {name: 'UAH', image: 'ua.svg'},
+    {name: 'EUR', image: './assets/images/R.png'},
+    {name: 'USD', image: 'us.svg'},
+    {name: 'GBP', image: 'gb.svg'},
+    {name: 'CAD', image: 'ca.svg'},
+    {name: 'JPY', image: 'jp.svg'},
   ];
 
   amount1: string = '0';
